6b26207b3d5d14f2fb2ac2e7e25f7b0fafd972c4
Crabbycraboo <aleinafayeg@gmail.com>
2026-05-31 18:06:42 +0800

    Update database.js

--- sams-wifi-v3/db/database.js
+++ sams-wifi-v3/db/database.js
@@ -1,8 +1,9 @@
 const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.SUPABASE_URL || 'undefined';
+const supabaseKey = process.env.SUPABASE_ANON_KEY || 'undefined';
 
+// We won't throw an error here, so the server can at least boot up
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 module.exports = { supabase };
