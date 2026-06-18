3344e0f476e19316948c78f994b6fd4de3258bd9
Crabbycraboo <aleinafayeg@gmail.com>
2026-06-18 16:39:25 +0800

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('trust proxy', 1);
 app.use(session({
-  secret: process.env.SESSION_SECRET || 'taytay-sams-wifi-2024',
+  secret: process.env.SESSION_SECRET || '39ers-cainta-2024',
   resave: false,
   saveUninitialized: false,
   rolling: true,
